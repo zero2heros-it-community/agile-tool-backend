f7bee95d9bb09ebff17d35e30ca11e25d14913b2
sever <haticeseverr@hotmail.com>
2023-12-26 14:55:08 +0100

    getWorkspace method created and NotFoundException added

--- application-service/src/main/java/org/zero2hero/applicationservice/exception/NotFoundException.java
+++ application-service/src/main/java/org/zero2hero/applicationservice/exception/NotFoundException.java
@@ -1,9 +1,8 @@
 package org.zero2hero.applicationservice.exception;
 
-public class NotFoundException extends RuntimeException {
+public class NotFoundException extends RuntimeException{
 
-    public NotFoundException(String message) {
+    public NotFoundException(String message){
         super(message);
     }
 }
-
