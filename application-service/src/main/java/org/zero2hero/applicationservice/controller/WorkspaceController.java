5e253a18b996c4d55c70b741df9bda00ba590351
sever <haticeseverr@hotmail.com>
2023-12-26 14:55:08 +0100

    getWorkspace method created and NotFoundException added

--- application-service/src/main/java/org/zero2hero/applicationservice/controller/WorkspaceController.java
+++ application-service/src/main/java/org/zero2hero/applicationservice/controller/WorkspaceController.java
@@ -3,13 +3,14 @@ package org.zero2hero.applicationservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zero2hero.applicationservice.dto.WorkspaceCreateDto;
 import org.zero2hero.applicationservice.dto.WorkspaceViewDto;
+import org.zero2hero.applicationservice.entity.Workspace;
+import org.zero2hero.applicationservice.exception.NotFoundException;
 import org.zero2hero.applicationservice.service.WorkspaceService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/work-space")
@@ -24,4 +25,20 @@ public class WorkspaceController {
                 workspaceCreateDto);
         return new ResponseEntity<>(workspaceViewDto, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getWorkspace(@PathVariable Long id) {
+        if (id == null || id <= 0) {
+            return new ResponseEntity<>(Map.of("message", "workspace Id is in incorrect format!",
+            "code", HttpStatus.BAD_REQUEST.value()),HttpStatus.BAD_REQUEST);
+        }
+        try {
+            Workspace workspace = workspaceService.getById(id);
+            return new ResponseEntity<>(workspace, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(Map.of("message", "Workspace not found"), HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 }
--- application-service/src/main/java/org/zero2hero/applicationservice/service/WorkspaceService.java
+++ application-service/src/main/java/org/zero2hero/applicationservice/service/WorkspaceService.java
@@ -3,7 +3,10 @@ package org.zero2hero.applicationservice.service;
 
 import org.zero2hero.applicationservice.dto.WorkspaceCreateDto;
 import org.zero2hero.applicationservice.dto.WorkspaceViewDto;
+import org.zero2hero.applicationservice.entity.Workspace;
 
 public interface WorkspaceService {
     WorkspaceViewDto create(WorkspaceCreateDto workspaceCreateDto);
+
+    Workspace getById(Long id);
 }
