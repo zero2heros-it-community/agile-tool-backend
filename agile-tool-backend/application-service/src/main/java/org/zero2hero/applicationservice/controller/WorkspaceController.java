05e92cfb9dc817dad16b3a720e5398b962ece125
Cihat TURHAN <cihatturhan@gmail.com>
2024-05-12 12:51:17 +0200

    Code optimization

--- agile-tool-backend/application-service/src/main/java/org/zero2hero/applicationservice/controller/WorkspaceController.java
+++ agile-tool-backend/application-service/src/main/java/org/zero2hero/applicationservice/controller/WorkspaceController.java
@@ -11,9 +11,7 @@ import org.zero2hero.applicationservice.service.WorkspaceService;
 import org.zero2hero.applicationservice.util.LoggedUsername;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
